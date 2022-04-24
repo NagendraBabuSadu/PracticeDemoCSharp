c199f1509c11da8065e9ab2701fc0060724708e1
Babu Nagendra <nagendrababusadu@outlook.com>
2022-04-24 10:59:43 +0530

    Constructor Default and Parameterized

---
+++ PracticeDemoCSharp/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDemoCSharp.Models
+{
+    class Employee
+    {
+        public int empId;
+        public string empName;
+        public float empSalary;
+
+        public Employee()
+        {
+            Console.WriteLine("Default constructor is called...");
+        }
+
+        public Employee(int id, string name, float salary)
+        {
+            this.empId = id;
+            this.empName = name;
+            this.empSalary = salary;
+            Console.WriteLine("Parameterized constructor is called");
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("ID : " + empId);
+            Console.WriteLine("Name : "+ empName);
+            Console.WriteLine("Salary : " + empSalary);
+
+        }
+
+    }
+
+
+}
--- PracticeDemoCSharp/Program.cs
+++ PracticeDemoCSharp/Program.cs
@@ -11,29 +11,15 @@ namespace PracticeDemoCSharp
     {
         static void Main(string[] args)
         {
-            Program p = new Program();
-            int[] nums = { 1, 2, 3, 4, 5 };
-            // p.Show(nums);
-             object[] ob = { "King", 123, 'a', 3.14 };
-            p.ShowObjects(ob);
+
+            Employee emp = new Employee();
+            emp.ShowData();
+            Employee emp1 = new Employee(121, "Fahad", 12300);
+            emp1.ShowData();
 
 
 
-            /*
-            int[][] jaggedArray = new int[2][];
-            jaggedArray[0] = new int[] { 11, 12, 43, 23, 53, 56 };
-            jaggedArray[1] = new int[] { 1, 2, 3, 4, 2, 2, 6, 7, 0 };
 
-            for (int i = 0; i < jaggedArray.Length; i++)
-            {
-                for (int j = 0; j < jaggedArray[i].Length; j++)
-                {
-                    Console.Write(jaggedArray[i][j] +" \t");
-                   
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine(jaggedArray[0][1]);
 
             Console.ReadLine();
 
